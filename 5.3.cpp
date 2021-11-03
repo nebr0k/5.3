42c7f827ed48629c84da72ce8b933738ddb0a773
nebr0k <90468237+nebr0k@users.noreply.github.com>
2021-11-03 15:45:16 +0200

    Add files via upload

---
+++ 5.3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double z(const double x);
+int main()
+{
+	double Pp, Pk, o ;
+	int n;
+	cout << "Pp = "; cin >> Pp;
+	cout << "Pk = "; cin >> Pk;
+	cout << "n = "; cin >> n;
+	double dp = (Pk - Pp) / n;
+	double P = Pp;
+	while (P <= Pk)
+	{
+
+		o = z(P*P+1) - z(P*P-1)+2*z(P);
+		cout << P << " " << o << endl;
+		P += dp;
+	}
+	return 0;
+}
+double z(const double x)
+{
+	if (abs(x) >= 1)
+		return (sin(x) + 1) / (pow(cos(x), 2)+exp(x));
+	else
+	{
+		double S = 0;
+		int k = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			k++;
+			double R = 2*x/k;
+			a *= R;
+			S += a;
+		} while (k < 6);
+		S = 1.0 / exp(x * x) * S;
+	   return S;
+	}
+	
+}
